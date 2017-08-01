b7668fb94eb0f2829e1ecb3c7eb5b499ff270155
Jason Mann <aztuxmann+github@gmail.com>
2017-08-01 06:11:00 -0700

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -9,19 +9,19 @@
 
 int main()
 {
-	int fib_one = 0;    // first number in the sequence.
-	int fib_two = 1;    // second number in the sequence.
+	int fib_one = 0;    	// first number in the sequence.
+	int fib_two = 1;    	// second number in the sequence.
 	int new_fib_two = 0;    // placeholder for next number in the sequence.
 
     printf("The first six digits in the Fibonacci sequence are: ");
 	printf("%d, ", fib_one);  // print first number in the sequence.
 	printf("%d, ", fib_two);  // print second number in the sequence.
 
-	for (int i = 0; i < 4; i++) {   // Compute the next four numbers
+	for (int i = 0; i < 4; i++) {   	    // Compute the next four numbers
 		new_fib_two = fib_one + fib_two;    // sum of the two preceding numbers.
-		printf("%d, ", new_fib_two);    // print the next number in the sequence.
-		fib_one = fib_two;          // Shift the numbers out.
-		fib_two = new_fib_two;      // Shift the numbers out.
+		printf("%d, ", new_fib_two);        // print the next number in the sequence.
+		fib_one = fib_two;                  // Shift the numbers out.
+		fib_two = new_fib_two;              // Shift the numbers out.
 		}
 
 	return 0;   // End proggram.
